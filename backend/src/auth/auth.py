4b2d07e6760020f2c412ff7a1b1cba44018ae808
Mohammad Al-Haddad <MohammadxCoder@icloud.com>
2020-12-31 13:04:05 +0300

    Fixed Rasing Errors in Check_permissions method

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -40,16 +40,10 @@ def get_token_auth_header():
 
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
-        raise AuthError({
-            'code': 'invalid_claims',
-            'description': 'Permissions not included in JWT.'
-        }, 400)
+        raise AuthError(400)
 
     if permission not in payload['permissions']:
-        raise AuthError({
-            'code': 'unauthorized',
-            'description': 'Permission not found.'
-        }, 403)
+        raise AuthError(403)
     return True
 
 
