d1f04e46a4ae41c3a5d92417244ed29884951082
Mohammad Al-Haddad <MohammadxCoder@icloud.com>
2021-01-05 01:37:16 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fsnd-udacity.eu.auth0.com', // the auth0 domain prefix
+    url: 'fsnd-udacity.eu', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'BIOV54fFm2llcXppIf7CMRA56SZE7VNK', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
